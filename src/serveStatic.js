2b27473ae7be27eec08f3c2559b4d645ad40586e
David Sobral <david.sobral@valtech.com>
2022-11-27 09:23:05 -0300

    Added export

--- src/serveStatic.js
+++ src/serveStatic.js
@@ -2,6 +2,10 @@ const express = require('express');
 const dirname = require('./getPath')
 
 // initialize server
-const server = express();
-server.use(express.static(dirname + '/public'));
-server.listen(3001, () => {console.log('Listening on http://localhost:3001/')});
+async function serveStatic() {
+    const server = express();
+    server.use(express.static(dirname + '/public'));
+    server.listen(3001, () => {console.log('Listening on http://localhost:3001/')});
+}
+
+module.exports = serveStatic
